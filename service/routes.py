1c65d95af9da13689a78b2a5b7176653cf403801
sfarl <1084091828@qq.com>
2023-03-23 11:20:17 -0400

    get all shopcarts

--- service/routes.py
+++ service/routes.py
@@ -31,11 +31,48 @@ def index():
 ######################################################################
 
 # Place your REST API code here ...
+######################################################################
+# LIST ALL Shopcarts
+######################################################################
+@app.route("/shopcarts", methods=['GET'])
+def list_shopcart_items(customer_id):
+    """
+    Retrieve all shopcarts in DB
+    This endpoint will return all the shopcart
+    Args:
 
+    Returns:
+        shopcart_id (int): id of the shopcart
+        customer_id (int): id of the customer who owns the shopcart
+    """
+
+    app.logger.info("Request for all shopcart")
+    results = ShopCarts.all_shopcarts()
+
+    res = {}
+    shopcart_list = []
+
+    for record in results:
+        current_shopcart = record.serialize()
+        shopcart = {
+            'shopcart_id': current_shopcart['id'],
+            'customer_id': current_shopcart['customer_id'],
+            # 'shopcart_record_id': current_item['id']
+            # will return price after integrating with product
+        }
+        shopcart_list.append(shopcart)
+
+    app.logger.info(
+        "Returning %d shopcart ", len(shopcart_list['items'])
+    )
+    res['shopcart_lists'] = shopcart_list
+    return jsonify(res), status.HTTP_200_OK
 
 # -----------------------------------------------------------
 # Create counters
 # -----------------------------------------------------------
+
+
 @app.route("/shopcarts/<customer_id>/<item_id>", methods=["POST"])
 def add_item(customer_id, item_id):
     """Creates a new entry and stores it in the database
@@ -45,18 +82,23 @@ def add_item(customer_id, item_id):
     Returns:
         dict: the row entry in databse which contains customer_id, item_id and quantity default to 1
     """
-    app.logger.info(f"Request to add item for customer {customer_id} and item {item_id}")
+    app.logger.info(
+        f"Request to add item for customer {customer_id} and item {item_id}")
     if customer_id is None or item_id is None or not customer_id.isdigit() or not item_id.isdigit():
-        abort(status.HTTP_400_BAD_REQUEST, f"Bad request for {customer_id} {item_id}")
+        abort(status.HTTP_400_BAD_REQUEST,
+              f"Bad request for {customer_id} {item_id}")
 
     customer_id = int(customer_id)
     item_id = int(item_id)
 
     if ShopCarts.check_exist_by_customer_id_and_product_id(customer_id, item_id):
-        logger.info(f"Customer {customer_id} and coresponding ietm {item_id} already exists")
-        abort(status.HTTP_409_CONFLICT, f"Customer {customer_id} and coresponding ietm {item_id} already exists")
+        logger.info(
+            f"Customer {customer_id} and coresponding ietm {item_id} already exists")
+        abort(status.HTTP_409_CONFLICT,
+              f"Customer {customer_id} and coresponding ietm {item_id} already exists")
 
-    shopcart = ShopCarts(customer_id=customer_id, product_id=item_id, quantities=1)
+    shopcart = ShopCarts(customer_id=customer_id,
+                         product_id=item_id, quantities=1)
     shopcart.create()
     logger.info(f"Added item {item_id} for customer {customer_id} sucessfully")
     return (
@@ -83,7 +125,8 @@ def list_shopcart_items(customer_id):
             quantity (int): number of the product in the cart
     """
 
-    app.logger.info("Request for shopcart items of customer with id: %s", customer_id)
+    app.logger.info(
+        "Request for shopcart items of customer with id: %s", customer_id)
     results = ShopCarts.find_by_customer_id(customer_id)
 
     shopcart_list = {}
@@ -117,25 +160,32 @@ def list_shopcart_items(customer_id):
 @app.route("/shopcarts/<int:customer_id>/<int:product_id>/<int:quantity>", methods=["PUT"])
 def update_shopcart_item(customer_id, product_id, quantity):
     """Updates the quantity of an existing product"""
-    app.logger.info(f"Update product-{product_id} in customer-{customer_id}'s cart to {quantity}")
+    app.logger.info(
+        f"Update product-{product_id} in customer-{customer_id}'s cart to {quantity}")
 
     product_id = int(product_id)
     customer_id = int(customer_id)
     quantity = int(quantity)
 
     if quantity <= 0:
-        app.logger.error(f"Quantity to be updated [{quantity}] should be positive!")
-        abort(status.HTTP_400_BAD_REQUEST, f"Quantity to be updated [{quantity}] should be positive!")
+        app.logger.error(
+            f"Quantity to be updated [{quantity}] should be positive!")
+        abort(status.HTTP_400_BAD_REQUEST,
+              f"Quantity to be updated [{quantity}] should be positive!")
 
-    shopcart_item = ShopCarts.find_by_customer_id_and_product_id(customer_id, product_id)
+    shopcart_item = ShopCarts.find_by_customer_id_and_product_id(
+        customer_id, product_id)
 
     if not shopcart_item:
-        app.logger.error(f"Product-{product_id} doesn't exist in customer-{customer_id}'s cart!")
-        abort(status.HTTP_404_NOT_FOUND, f"Product-{product_id} doesn't exist in the customer-{customer_id}'s cart!")
+        app.logger.error(
+            f"Product-{product_id} doesn't exist in customer-{customer_id}'s cart!")
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Product-{product_id} doesn't exist in the customer-{customer_id}'s cart!")
 
     shopcart_item.quantities = quantity
     shopcart_item.update()
-    app.logger.info(f"Updated Product-{product_id} quantity to {quantity} in customer-{customer_id}'s cart!")
+    app.logger.info(
+        f"Updated Product-{product_id} quantity to {quantity} in customer-{customer_id}'s cart!")
 
     return jsonify(shopcart_item.serialize()), status.HTTP_200_OK
 
@@ -152,34 +202,42 @@ def delete_shopcart_item(customer_id, product_id):
     product_id = int(product_id)
     customer_id = int(customer_id)
 
-    shopcart_item = ShopCarts.find_by_customer_id_and_product_id(customer_id, product_id)
+    shopcart_item = ShopCarts.find_by_customer_id_and_product_id(
+        customer_id, product_id)
 
     if not shopcart_item:
-        app.logger.error(f"Product-{product_id} doesn't exist in customer-{customer_id}'s cart!")
-        abort(status.HTTP_404_NOT_FOUND, f"Product-{product_id} doesn't exist in the customer-{customer_id}'s cart!")
+        app.logger.error(
+            f"Product-{product_id} doesn't exist in customer-{customer_id}'s cart!")
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Product-{product_id} doesn't exist in the customer-{customer_id}'s cart!")
 
     shopcart_item.delete()
-    app.logger.info(f"Deleted Product-{product_id} in customer-{customer_id}'s cart!")
+    app.logger.info(
+        f"Deleted Product-{product_id} in customer-{customer_id}'s cart!")
 
     return jsonify(shopcart_item.serialize()), status.HTTP_200_OK
 
 #####################################################################
 # READ AN ITEM FROM A SHOPCART
 ######################################################################
+
+
 @app.route("/shopcarts/<int:customer_id>/<int:product_id>", methods=["GET"])
 def get_item(customer_id, product_id):
     """
     Read an item from a shopcart
     """
-    app.logger.info(f"Request to read an Item-{product_id} from Customer-{customer_id} 's shopcart")
+    app.logger.info(
+        f"Request to read an Item-{product_id} from Customer-{customer_id} 's shopcart")
     # Read an item with item_id
-    result = ShopCarts.find_by_customer_id_and_product_id(customer_id, product_id)
+    result = ShopCarts.find_by_customer_id_and_product_id(
+        customer_id, product_id)
     if result is not None:
         message = result.serialize()
         return jsonify(result.serialize()), status.HTTP_200_OK
     # See if the item exists and abort if it doesn't
-    else: 
-        logger.error(f"Customer {customer_id} and corresponding item {product_id} could not be found.")
-        abort(status.HTTP_404_NOT_FOUND, f"Customer {customer_id} and corresponding item {product_id} could not be found.")
-
-    
+    else:
+        logger.error(
+            f"Customer {customer_id} and corresponding item {product_id} could not be found.")
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Customer {customer_id} and corresponding item {product_id} could not be found.")
